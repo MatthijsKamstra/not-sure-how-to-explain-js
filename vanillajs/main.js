26ec7c1f5de94bb57551f051ba227bc70582f31a
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-03-04 11:36:56 +0100

    update calls

--- vanillajs/main.js
+++ vanillajs/main.js
@@ -2,11 +2,6 @@ function hello(name) {
     console.log('Hello ' + name + '!');
 }
 
-function changeContent(text) {
-    var el = document.getElementById('myTarget');
-    el.innerText = text;
-}
-
 function searchID(id) {
     var el = document.getElementById(id); // only one
     console.log(el);
@@ -18,11 +13,25 @@ function searchClassName(name) {
     console.log(arr.length);
 }
 
+function changeContent(text) {
+    var el = document.getElementById('myTarget');
+    el.innerText = text;
+}
+
+function changeBtnText(className, text) {
+    var arr = document.getElementsByClassName(className); // a list
+    for (let i = 0; i < arr.length; i++) {
+        const el = arr[i];
+        el.innerText = text;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
     // the event occurred
     console.log('DOM ready');
-    hello('Matthijs');
-    changeContent('this is dynamic content');
-    searchID('myTarget');
-    searchClassName('btn');
+    // hello('Matthijs');
+    // searchID('myTarget');
+    // searchClassName('btn');
+    // changeContent('this is dynamic content');
+    // changeBtnText('btn', 'Changed!');
 });
