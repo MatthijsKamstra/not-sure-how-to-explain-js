5d508b00ccc355f0a8ff5d5b89594dcc3ccba49e
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-03-04 11:44:01 +0100

    some examples

--- search/main.js
+++ search/main.js
@@ -1,9 +1,30 @@
+var obj = {
+	firstName: "Matthijs",
+	lastName: "Kamstra"
+};
 
+function init() {
+	findAndUpdate();
+}
 
+function changeContent() {
+	var el = document.getElementById('myTarget');
+	el.innerText = obj.firstName;
+	el.style.border = '1px solid pink';
+	el.title = 'changed';
+
+}
+
+function findAndUpdate() {
+	var btn = document.getElementById('changeContentBtn');
+	btn.onclick = function () {
+		changeContent();
+	}
+}
 
 
 document.addEventListener('DOMContentLoaded', function (event) {
 	// the event occurred
 	console.log('DOM ready');
-	changeContent('this is dynamic content');
+	init();
 });
