77955a985a37175ce7b2e397a2d85f42a19046af
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-03-04 10:56:03 +0100

    search and change values

--- vanillajs/main.js
+++ vanillajs/main.js
@@ -1,7 +1,28 @@
+function hello(name) {
+    console.log('Hello ' + name + '!');
+}
 
+function changeContent(text) {
+    var el = document.getElementById('myTarget');
+    el.innerText = text;
+}
 
+function searchID(id) {
+    var el = document.getElementById(id); // only one
+    console.log(el);
+}
+
+function searchClassName(name) {
+    var arr = document.getElementsByClassName(name); // a list
+    console.log(arr);
+    console.log(arr.length);
+}
 
 document.addEventListener('DOMContentLoaded', function (event) {
     // the event occurred
     console.log('DOM ready');
-})
+    hello('Matthijs');
+    changeContent('this is dynamic content');
+    searchID('myTarget');
+    searchClassName('btn');
+});
