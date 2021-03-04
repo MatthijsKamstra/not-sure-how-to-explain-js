93ae5f5922b36bac494be9fd4fd58a8a0748bdd2
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-03-04 09:16:38 +0100

    initial commit

---
+++ basic/main.js
@@ -0,0 +1,11 @@
+console.log('hello');
+
+console.warn('warn');
+
+console.info('info');
+
+console.error('error');
+
+function test(params) {
+    console.log('test' + params);
+}
---
+++ vanillajs/main.js
@@ -0,0 +1,7 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    // the event occurred
+    console.log('DOM ready');
+})
